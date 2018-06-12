1db2fd21f7076d88710e249d58af2abd7017b842
hexoul <crosien@gmail.com>
2018-06-12 15:56:28 +0900

    Imp. and verify SendTransaction(), SendTransactionWithSign(), Call() at abi package

--- abi/abi.go
+++ abi/abi.go
@@ -2,14 +2,21 @@ package abi
 
 import (
 	"encoding/hex"
+	"math/big"
 	"strings"
 
+	"bitbucket.org/coinplugin/proxy/crypto"
 	"bitbucket.org/coinplugin/proxy/json"
 	"bitbucket.org/coinplugin/proxy/rpc"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rlp"
 )
+
+const Targetnet = rpc.Testnet
 
 func Pack(abi abi.ABI, name string, args ...interface{}) (string, error) {
 	data, err := abi.Pack(name, args...)
@@ -35,20 +42,65 @@ func Unpack(abi abi.ABI, v interface{}, name string, output string) error {
 	return abi.Unpack(v, name, data)
 }
 
-func Call(abi abi.ABI, to, name string, inputs []interface{}, outputs interface{}) error {
+func Call(abi abi.ABI, to, name string, inputs []interface{}, outputs interface{}) (resp json.RpcResponse, err error) {
 	data, err := Pack(abi, name, inputs...)
 	if err != nil {
-		return err
+		return
 	}
 
-	r := rpc.GetInstance(rpc.Testnet)
+	r := rpc.GetInstance(Targetnet)
 	respStr, err := r.Call(to, data)
 	if err != nil {
-		return err
+		return
 	}
 
-	resp := json.GetRpcResponseFromJson(respStr)
-	return Unpack(abi, outputs, name, resp.Result.(string))
+	resp = json.GetRpcResponseFromJson(respStr)
+	return
+}
+
+func SendTransaction(abi abi.ABI, to, name string, inputs []interface{}, gas int) (resp json.RpcResponse, err error) {
+	data, err := Pack(abi, name, inputs...)
+	if err != nil {
+		return
+	}
+
+	c := crypto.GetInstance()
+	r := rpc.GetInstance(Targetnet)
+	respStr, err := r.SendTransaction(c.Address, to, data, gas)
+	if err != nil {
+		return
+	}
+
+	resp = json.GetRpcResponseFromJson(respStr)
+	return
+}
+
+func SendTransactionWithSign(abi abi.ABI, to, name string, inputs []interface{}, gasLimit, gasPrice uint64) (resp json.RpcResponse, err error) {
+	data, err := abi.Pack(name, inputs...)
+	if err != nil {
+		return
+	}
+
+	c := crypto.GetInstance()
+	tx := types.NewTransaction(0, common.HexToAddress(to), big.NewInt(0), uint64(gasLimit), big.NewInt(int64(gasPrice)), data)
+	tx, err = c.SignTx(tx)
+	if err != nil {
+		return
+	}
+
+	rlpTx, err := rlp.EncodeToBytes(tx)
+	if err != nil {
+		return
+	}
+
+	r := rpc.GetInstance(Targetnet)
+	respStr, err := r.SendRawTransaction(rlpTx)
+	if err != nil {
+		return
+	}
+
+	resp = json.GetRpcResponseFromJson(respStr)
+	return
 }
 
 func GetAbiFromJson(raw string) (abi.ABI, error) {
@@ -58,7 +110,7 @@ func GetAbiFromJson(raw string) (abi.ABI, error) {
 // getAbiFromAddress is NOT YET SUPPORTED
 // TODO: use eth.compile.solidity?
 func getAbiFromAddress(addr string) (abi abi.ABI) {
-	r := rpc.GetInstance(rpc.Testnet)
+	r := rpc.GetInstance(Targetnet)
 	respStr, err := r.GetCode(addr)
 	if err != nil {
 		return
--- abi/abi_test.go
+++ abi/abi_test.go
@@ -1,19 +1,105 @@
 package abi
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+const testabijson = `
+[{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"}]
+`
+
+const testabijson2 = `
+[
+	{ "type" : "function", "name" : "balance", "constant" : true },
+	{ "type" : "function", "name" : "send", "constant" : false, "inputs" : [ { "name" : "amount", "type" : "uint256" } ] },
+	{ "type" : "function", "name" : "test", "constant" : false, "inputs" : [ { "name" : "number", "type" : "uint32" } ] },
+	{ "type" : "function", "name" : "string", "constant" : false, "inputs" : [ { "name" : "inputs", "type" : "string" } ] },
+	{ "type" : "function", "name" : "bool", "constant" : false, "inputs" : [ { "name" : "inputs", "type" : "bool" } ] },
+	{ "type" : "function", "name" : "address", "constant" : false, "inputs" : [ { "name" : "inputs", "type" : "address" } ] },
+	{ "type" : "function", "name" : "uint64[2]", "constant" : false, "inputs" : [ { "name" : "inputs", "type" : "uint64[2]" } ] },
+	{ "type" : "function", "name" : "uint64[]", "constant" : false, "inputs" : [ { "name" : "inputs", "type" : "uint64[]" } ] },
+	{ "type" : "function", "name" : "foo", "constant" : false, "inputs" : [ { "name" : "inputs", "type" : "uint32" } ] },
+	{ "type" : "function", "name" : "bar", "constant" : false, "inputs" : [ { "name" : "inputs", "type" : "uint32" }, { "name" : "string", "type" : "uint16" } ] },
+	{ "type" : "function", "name" : "slice", "constant" : false, "inputs" : [ { "name" : "inputs", "type" : "uint32[2]" } ] },
+	{ "type" : "function", "name" : "slice256", "constant" : false, "inputs" : [ { "name" : "inputs", "type" : "uint256[2]" } ] },
+	{ "type" : "function", "name" : "sliceAddress", "constant" : false, "inputs" : [ { "name" : "inputs", "type" : "address[]" } ] },
+	{ "type" : "function", "name" : "sliceMultiAddress", "constant" : false, "inputs" : [ { "name" : "a", "type" : "address[]" }, { "name" : "b", "type" : "address[]" } ] }
+]`
 
 var (
 	testcontractaddr = "0xc6f1fbb70f850c981591f65f73cd158fb38b6807"
 
-	testabi = "[{\"constant\":true,\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"transferOwnership\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"previousOwner\",\"type\":\"address\"},{\"indexed\":true,\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"OwnershipTransferred\",\"type\":\"event\"}]"
+	testaddr = "0xd396348325532a21ab2b01aeee1499a713453e7c"
 )
 
 func TestGetAbiFromJson(t *testing.T) {
-	abi, err := GetAbiFromJson(testabi)
+	_, err := GetAbiFromJson(testabijson2)
 	if err != nil {
-		t.Errorf("Failed to getAbiFromJson")
+		t.Errorf("Failed to GetAbiFromJson: %s", err)
 	}
-	t.Logf("%v", abi)
+}
+
+func TestPack(t *testing.T) {
+	abi, err := GetAbiFromJson(testabijson2)
+	if err != nil {
+		t.Errorf("Failed to GetAbiFromJson: %s", err)
+	}
+
+	addr := common.HexToAddress(testaddr[2:])
+	data, err := Pack(abi, "address", addr)
+	if err != nil {
+		t.Errorf("Failed to Pack: %s", err)
+	}
+	t.Logf("data: %s", data)
+}
+
+func TestCall(t *testing.T) {
+	abi, err := GetAbiFromJson(testabijson)
+	if err != nil {
+		t.Errorf("Failed to GetAbiFromJson")
+	}
+
+	resp, err := Call(abi, testcontractaddr, "owner", []interface{}{}, 0x1)
+	if err != nil || resp.Result == "" || resp.Error.Code != 0 {
+		t.Errorf("Failed to Call")
+	}
+	t.Logf("%s", resp.String())
+
+	var addr common.Address
+	Unpack(abi, &addr, "owner", resp.Result.(string))
+	if len(addr) == 0 {
+		t.Errorf("Failed to Unpack")
+	}
+}
+
+func TestSendTransaction(t *testing.T) {
+	abi, err := GetAbiFromJson(testabijson)
+	if err != nil {
+		t.Errorf("Failed to GetAbiFromJson")
+	}
+
+	addr := common.HexToAddress(testaddr[2:])
+	resp, err := DummySendTransaction(abi, testcontractaddr, "transferOwnership", []interface{}{addr}, 0x1)
+	if err != nil || resp.Result == "" || resp.Error.Code != 0 {
+		t.Errorf("Failed to SendTransaction")
+	}
+	t.Logf("%s", resp.String())
+}
+
+func TestSendTransactionWithSign(t *testing.T) {
+	abi, err := GetAbiFromJson(testabijson)
+	if err != nil {
+		t.Errorf("Failed to GetAbiFromJson")
+	}
+
+	addr := common.HexToAddress(testaddr[2:])
+	resp, err := DummySendTransactionWithSign(abi, testcontractaddr, "transferOwnership", []interface{}{addr}, 0x1, 0x1)
+	if err != nil || resp.Result == "" || resp.Error.Code != 0 {
+		t.Errorf("Failed to SendTransactionWithSign")
+	}
+	t.Logf("%s", resp.String())
 }
 
 /*
