ea1f41c61fb1436e515085903fe8538f5373a24a
hexoul <crosien@gmail.com>
2018-06-19 11:53:58 +0900

    Link between main and predefined

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	_ "bitbucket.org/coinplugin/proxy/crypto"
 	"bitbucket.org/coinplugin/proxy/json"
+	"bitbucket.org/coinplugin/proxy/predefined"
 	"bitbucket.org/coinplugin/proxy/rpc"
-	"bitbucket.org/coinplugin/proxy/web3"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -26,37 +27,31 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 		req.Method = method
 	}
 
-	// Preprocessing
-	var unit string
-	if req.Method == "eth_getBalance" && len(req.Params) > 2 {
-		unit = req.Params[2].(string)
-		req.Params = req.Params[:2]
-	}
-
-	// Forward RPC request to Ether node
-	respBody, err := rpc.GetInstance(Targetnet).DoRpc(req)
-
-	// Relay a response from the node
-	resp := json.GetRpcResponseFromJson(respBody)
-
-	// Postprocessing
-	if unit != "" {
-		if val, err := web3.FromWei(resp.Result.(string), unit); err == nil {
-			resp.Result = val
+	var resp json.RpcResponse
+	var err error
+	if predefined.Contains(req.Method) {
+		// Forward RPC request to predefined function
+		resp, err = predefined.Forward(req)
+	} else {
+		// Forward RPC request to Ether node
+		respBody, err := rpc.GetInstance(Targetnet).DoRpc(req)
+		if err == nil {
+			// Relay a response from the node
+			resp = json.GetRpcResponseFromJson(respBody)
 		}
 	}
 
 	retCode := 200
 	if err != nil {
 		// In case of server-side RPC fail
+		fmt.Println(err.Error())
 		resp.Error.Message = err.Error()
-		respBody = resp.String()
 		retCode = 400
 	} else if resp.Error.Code != 0 {
 		// In case of ether-node-side RPC fail
 		retCode = 400
 	}
-	return events.APIGatewayProxyResponse{Body: respBody, StatusCode: retCode}, nil
+	return events.APIGatewayProxyResponse{Body: resp.String(), StatusCode: retCode}, nil
 }
 
 func main() {
