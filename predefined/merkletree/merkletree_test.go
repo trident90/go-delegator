2d1a05de9de73c14e0eb16f28d9a3f92d1221c4e
신용욱 <lineart@gmail.com>
2018-07-02 16:04:22 +0900

    테스트 코드 정리

--- predefined/merkletree/merkletree_test.go
+++ predefined/merkletree/merkletree_test.go
@@ -4,28 +4,26 @@ import (
 	"fmt"
 	"testing"
 
-	proxyCrypto "bitbucket.org/coinplugin/proxy/crypto"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/crypto"
 )
 
-// //TestContent implements the Content interface provided by merkletree and represents the content stored in the tree.
-type TestContent struct {
-	x string
-}
+// // //TestContent implements the Content interface provided by merkletree and represents the content stored in the tree.
+// type TestContent struct {
+// 	x string
+// }
 
-//CalculateHash hashes the values of a TestContent
-func (t TestContent) CalculateHash() []byte {
-	// h := sha256.New()
-	// h.Write([]byte(t.x))
-	// return h.Sum(nil)
-	return crypto.Keccak256([]byte(t.x))
-}
+// //CalculateHash hashes the values of a TestContent
+// func (t TestContent) CalculateHash() []byte {
+// 	// h := sha256.New()
+// 	// h.Write([]byte(t.x))
+// 	// return h.Sum(nil)
+// 	return crypto.Keccak256([]byte(t.x))
+// }
 
-//Equals tests for equality of two Contents
-func (t TestContent) Equals(other Content) bool {
-	return t.x == other.(TestContent).x
-}
+// //Equals tests for equality of two Contents
+// func (t TestContent) Equals(other Content) bool {
+// 	return t.x == other.(TestContent).x
+// }
 
 func TestMerkleTree(t *testing.T) {
 
@@ -68,38 +66,5 @@ func TestMerkleTree(t *testing.T) {
 
 	//String representation
 	//fmt.Println(tree)
-
-}
-
-func TestSign(t *testing.T) {
-	//privKey := "348a1e57efc055f3b8243f6e78fb18d0508f68d6f2e7cadf92b87e84a0b51024"
-	privKey := "01b149603ca8f537bbb4e45d22e77df9054e50d826bb5f0a34e9ce460432b596"
-	key, _ := crypto.HexToECDSA(privKey)
-	pubKey := key.PublicKey
-	fmt.Println("pubkey : ", pubKey)
-	sAddress := crypto.PubkeyToAddress(pubKey)
-	fmt.Printf("sAddress : %x \n", sAddress)
-
-	bMsg := crypto.Keccak256([]byte("test"))
-	msg := hexutil.Encode(bMsg)
-	//msg := "test"
-	fmt.Println("msg : ", msg)
-	s, err := proxyCrypto.Sign("test", privKey)
-	if err != nil {
-		t.Error("Err", err)
-	}
-	s[64] += 27
-	fmt.Printf("sig : %x\n", s)
-
-	//sig := "0x1d42443d80dfba7a06f32b8496bb0689b2c6963c48ba1e7dfbb8d39976e1629f7ed75dc66c57c278ea44a49e13871592d57eb8f19606e7df1c34032ebf3c86ef1b"
-	sig := hexutil.Encode(s)
-	s2, err := proxyCrypto.EcRecover(msg, sig)
-	if err != nil {
-		t.Error("Err", err)
-	}
-
-	fmt.Println("sign Address : ", sAddress)
-
-	fmt.Println("ecrecover Address : ", s2)
 
 }
