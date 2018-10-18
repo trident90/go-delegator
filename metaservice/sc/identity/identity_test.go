3f0958bbe0815e9261ca700105a49466fca00473
신용욱 <lineart@gmail.com>
2018-10-18 16:55:35 +0900

    import Meta Identity Contract

---
+++ metaservice/sc/identity/identity_test.go
@@ -0,0 +1,125 @@
+package identity
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"testing"
+
+	"bitbucket.org/coinplugin/proxy/crypto"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func defaultSetting() {
+	path := "/tmp/testKey"
+
+	file, err := os.Open("/tmp/testKeyPass")
+	if err != nil {
+		fmt.Printf("fail reading file : %s \n", err)
+		panic("Fail to read key file")
+	}
+	defer file.Close()
+	r := bufio.NewReader(file)
+	data, _, err := r.ReadLine()
+	passphrase := string(data)
+
+	go func() { crypto.PathChan <- path }()
+	go func() { crypto.PassphraseChan <- passphrase }()
+	crypto.GetInstance()
+
+}
+func TestCallGetTransactionCount(t *testing.T) {
+	defaultSetting()
+
+	//mgtAddress := common.HexToAddress("0x961c20596e7ec441723fbb168461f4b51371d8aa") // SC: 0x1840c5049536874fcFfC62ed9E0fC6cE7b773c49
+	scAddress := common.HexToAddress("0x1840c5049536874fcFfC62ed9E0fC6cE7b773c49")
+
+	identity, err := GetInstance(scAddress)
+
+	if err != nil {
+		t.Error("Error get Identity", err)
+	}
+
+	nonce, err := CallGetTransactionCount(identity)
+
+	if err != nil {
+		t.Error("Error CallCreateMetaID", err)
+	}
+	fmt.Printf("Transaction count : %x \n", nonce)
+}
+
+func TestCallGetKey(t *testing.T) {
+	defaultSetting()
+	//mgtAddress := common.HexToAddress("0x0000000000000000000000000000000000000000")
+	keyAddress := common.HexToAddress("0xd3Cb37aE6a81EbF1b5C3D9422636b0dB48767B72") // SC: 0x1840c5049536874fcFfC62ed9E0fC6cE7b773c49
+	scAddress := common.HexToAddress("0xe052cb04e4fe4d3ca69d247b4eff2aff35613b0e")
+
+	instance, err := GetInstance(scAddress)
+
+	if err != nil {
+		t.Error("Error get Identity", err)
+	}
+	keyBytes := CallAddrToKey(keyAddress)
+	// if err != nil {
+	// 	t.Error("Error addrToKey", err)
+	// }
+	if keyBytes == *emptyKeyVal {
+		t.Error("Key empty")
+	}
+	fmt.Printf("mgt Key  : %x \n", keyBytes)
+	metaKey, err := CallGetKey(instance, keyBytes)
+	if err != nil {
+		t.Error("Error getKey", err)
+	}
+	fmt.Printf("Get Key : %x \n", metaKey)
+}
+
+func TestCallGetKeysByPurpose(t *testing.T) {
+	defaultSetting()
+
+	//mgtAddress := common.HexToAddress("0x961c20596e7ec441723fbb168461f4b51371d8aa") // SC: 0x1840c5049536874fcFfC62ed9E0fC6cE7b773c49
+	scAddress := common.HexToAddress("0xe052cb04e4fe4d3ca69d247b4eff2aff35613b0e")
+
+	instance, err := GetInstance(scAddress)
+
+	keys, err := CallGetKeysByPurpose(instance, common.Big2)
+	if err != nil {
+		t.Error("Error getKey", err)
+	}
+	fmt.Printf("GetKeysByPurpose : %x \n", keys)
+	for _, p := range keys {
+		//management
+		fmt.Printf("%x \n", p)
+	}
+
+}
+
+/*
+func TestCallDelegatedExecute(t *testing.T) {
+	defaultSetting()
+
+	mgtAddress := common.HexToAddress("0x961c20596e7ec441723fbb168461f4b51371d8aa")
+	scAddress := common.HexToAddress("0x1840c5049536874fcFfC62ed9E0fC6cE7b773c49")
+
+	identity, err := GetInstance(scAddress)
+	if err != nil {
+		t.Error("Error CallCreateMetaID", err)
+	}
+	value := big.NewInt(0)
+	data := hexutil.MustDecode("0x00")
+	nonce := big.NewInt(0)
+	sig := hexutil.MustDecode("0x00")
+
+
+	trx, err := CallDelegatedExecute(identity, mgtAddress, scAddress, value, data, executeID, sig)
+	if err != nil {
+		t.Error("Error CallCreateMetaID", err)
+	}
+	cryptoIns := crypto.GetInstance()
+	proxyAddress := common.HexToAddress(cryptoIns.GetAddress())
+
+	scAddress := ethCrypto.CreateAddress(proxyAddress, trx.Nonce())
+	fmt.Printf("Meta ID Address: %x \n", scAddress)
+	fmt.Printf("trxid : %v", trx.Hash().String())
+}
+*/
---
+++ metaservice/sc/identity/wrapper.go
@@ -0,0 +1,240 @@
+package identity
+
+import (
+	"bytes"
+	"fmt"
+	"math/big"
+	"sync"
+
+	"bitbucket.org/coinplugin/proxy/log"
+
+	"bitbucket.org/coinplugin/proxy/crypto"
+	"bitbucket.org/coinplugin/proxy/rpc"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+	//	"github.com/ethereum/go-ethereum/crypto"
+)
+
+var (
+	once   sync.Once
+	zero   = big.NewInt(0)
+	glimit = uint64(4000000)
+
+	recoveryFuncHash = hexutil.MustDecode("0x1d381240")
+	emptyKeyVal      = new([32]byte)
+)
+
+func GetInstance(address common.Address) (*Identity, error) {
+	_rpc := rpc.GetInstance()
+	client := _rpc.GetEthClient()
+
+	instance, err := NewIdentity(address, client)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	if instance == nil {
+		err := fmt.Errorf("Cannot get MetaID Instance")
+		return nil, err
+	}
+	return instance, nil
+}
+
+//CallDelegatedExecute DelegatedExecute function call
+func CallDelegatedExecute(instance *Identity, mgtAddress common.Address, to common.Address, value *big.Int, data hexutil.Bytes, metaNonce *big.Int, signature hexutil.Bytes) (*types.Transaction, error) {
+	var trx *types.Transaction
+	var err error
+
+	// instance, err := getInstance(mgtAddress)
+	// //session, err := getSession()
+
+	// if err != nil {
+	// 	log.Error(err)
+	// 	return nil, err
+	// }
+	if instance == nil {
+		err = fmt.Errorf("Error - Identity nil")
+		return nil, err
+	}
+
+	tx := func(nonce uint64) error {
+		_rpc := rpc.GetInstance()
+		auth := crypto.GetTransactionOpts()
+		auth.Nonce = big.NewInt(int64(nonce))
+		auth.GasPrice = big.NewInt(int64(_rpc.GetGasPrice()))
+		auth.GasLimit = glimit
+		log.Debug("gas price : ", auth.GasPrice)
+		trx, err = instance.DelegatedExecute(auth, to, value, data, metaNonce, signature)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	c := crypto.GetInstance()
+	res := c.ApplyNonce(tx)
+
+	if !res {
+		if err == nil {
+			err = fmt.Errorf("call function Error - CallDelegatedExecute")
+		}
+		return nil, err
+	}
+
+	return trx, nil
+}
+
+func CallGetTransactionCount(instance *Identity) (*big.Int, error) {
+	var err error
+	if instance == nil {
+		err = fmt.Errorf("Error - Identity nil")
+		return nil, err
+	}
+
+	result, err := instance.GetTransactionCount(&bind.CallOpts{})
+	if err != nil {
+		if err.Error() == "abi: unmarshalling empty output" {
+			return common.Big0, nil
+		}
+		log.Error(err)
+		return nil, err
+	}
+
+	log.Infof("Transction Count(%x): %x ", instance.Address, result)
+	return result, nil
+
+}
+
+func CheckDelegateExecutePermission(instance *Identity, from common.Address, to common.Address, data hexutil.Bytes) error {
+	var err error
+
+	//1. convert addr to key
+	keyBytes := CallAddrToKey(from)
+	// keyBytes, err := CallAddrToKey(instance, from)
+	// if err != nil {
+	// 	return err
+	// }
+	// if keyBytes == nil {
+	// 	return fmt.Errorf("fail to check Permission")
+	// }
+	//2. get Key
+	key, err := CallGetKey(instance, keyBytes)
+	if err != nil {
+		return err
+	}
+	if key == nil {
+		return fmt.Errorf("Not found key")
+	}
+	//3. check purpose
+	if bytes.Equal(instance.Address.Bytes(), to.Bytes()) { //For Management or Recovery
+		funcHash, err := CallGetFunctionSignature(instance, data)
+		if err != nil {
+			return err
+		}
+		for _, p := range key.Purposes {
+			//management
+			if common.Big1.Cmp(p) == 0 {
+				log.Debug("vaild permission for SELF - management Key")
+				return nil
+			} else if big.NewInt(7).Cmp(p) == 0 && bytes.Equal(recoveryFuncHash, funcHash[:]) {
+				log.Debug("vaild permission for SELF - recovery Key")
+				return nil
+			}
+		}
+	} else { //For Action
+		for _, p := range key.Purposes {
+			//management or action key
+			if common.Big1.Cmp(p) == 0 {
+				log.Debug("vaild permission - management Key")
+				return nil
+			} else if common.Big2.Cmp(p) == 0 {
+				log.Debug("vaild permission - action Key")
+				return nil
+			}
+		}
+	}
+	return fmt.Errorf("Not permission")
+}
+
+func CallGetFunctionSignature(instance *Identity, data hexutil.Bytes) (*[4]byte, error) {
+	var err error
+	if instance == nil {
+		err = fmt.Errorf("Error - Identity nil")
+		return nil, err
+	}
+
+	result, err := instance.GetFunctionSignature(&bind.CallOpts{}, data)
+	if err != nil {
+		//데이터가 없을때 나는 에러 처리
+		if err.Error() == "abi: unmarshalling empty output" {
+			return nil, nil
+		}
+		log.Error(err)
+		return nil, err
+	}
+	log.Debugf("functionSignature: %x ", result)
+	return &result, nil
+}
+
+func CallGetKey(instance *Identity, key [32]byte) (*metaIDKey, error) {
+	var err error
+	if instance == nil {
+		err = fmt.Errorf("Error - Identity nil")
+		return nil, err
+	}
+	result, err := instance.GetKey(&bind.CallOpts{}, key)
+	if err != nil {
+		//데이터가 없을때 나는 에러 처리
+		if err.Error() == "abi: unmarshalling empty output" {
+			return nil, nil
+		}
+		log.Error(err)
+		return nil, err
+	}
+	log.Debugf("key: %x ", result)
+	return &result, nil
+}
+
+func CallGetKeysByPurpose(instance *Identity, purpose *big.Int) ([][32]byte, error) {
+	var err error
+	if instance == nil {
+		err = fmt.Errorf("Error - Identity nil")
+		return nil, err
+	}
+	result, err := instance.GetKeysByPurpose(&bind.CallOpts{}, purpose)
+	if err != nil {
+		//데이터가 없을때 나는 에러 처리
+		if err.Error() == "abi: unmarshalling empty output" {
+			return nil, nil
+		}
+		log.Error(err)
+		return nil, err
+	}
+	log.Debugf("keys: %x ", result)
+	return result, nil
+}
+
+/*
+func CallAddrToKey(instance *Identity, address common.Address) ([32]byte, error) {
+
+	var err error
+	if instance == nil {
+		err = fmt.Errorf("Error - Identity nil")
+		return *emptyKeyVal, err
+	}
+	result, err := instance.AddrToKey(&bind.CallOpts{}, address)
+	if err != nil {
+		log.Error(err)
+		return *emptyKeyVal, err
+	}
+	log.Debugf("address to key: %x ", result)
+	return result, nil
+}
+*/
+func CallAddrToKey(address common.Address) [32]byte {
+	var result [32]byte
+	copy(result[12:], address.Bytes())
+	return result
+}
