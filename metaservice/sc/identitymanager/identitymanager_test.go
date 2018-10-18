bdabe598ce4b2907e4912903694aa1ef926268aa
신용욱 <lineart@gmail.com>
2018-10-18 16:55:06 +0900

    import Identity manager Contract

---
+++ metaservice/sc/identitymanager/identitymanager_test.go
@@ -0,0 +1,66 @@
+package identitymanager
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"testing"
+
+	"bitbucket.org/coinplugin/proxy/crypto"
+	"github.com/ethereum/go-ethereum/common"
+	ethCrypto "github.com/ethereum/go-ethereum/crypto"
+)
+
+func defaultSetting() {
+	path := "/tmp/testKey"
+
+	file, err := os.Open("/tmp/testKeyPass")
+	if err != nil {
+		fmt.Printf("fail reading file : %s \n", err)
+		panic("Fail to read key file")
+	}
+	defer file.Close()
+	r := bufio.NewReader(file)
+	data, _, err := r.ReadLine()
+	passphrase := string(data)
+
+	go func() { crypto.PathChan <- path }()
+	go func() { crypto.PassphraseChan <- passphrase }()
+	crypto.GetInstance()
+
+}
+
+func TestCallCreateMetaID(t *testing.T) {
+	defaultSetting()
+
+	address := common.HexToAddress("0x961c20596e7ec441723fbb168461f4b51371d8aa") //e052cb04e4fe4d3ca69d247b4eff2aff35613b0e
+	trx, err := CallCreateMetaID(address)
+	if err != nil {
+		t.Error("Error CallCreateMetaID", err)
+	}
+
+	scAddress := ethCrypto.CreateAddress(*trx.To(), trx.Nonce())
+	fmt.Printf("Meta ID Address: %x \n", scAddress)
+	fmt.Printf("trxid : %v", trx.Hash().String())
+}
+
+func TestCallGetDeployedMetaIds(t *testing.T) {
+	defaultSetting()
+	addrs, err := CallGetDeployedMetaIds()
+
+	if err != nil {
+		t.Error("Error - ", err)
+	}
+	fmt.Println(addrs)
+	for _, p := range addrs {
+		//management
+		fmt.Printf("%x \n", p)
+	}
+
+}
+
+func TestCreateAddress(t *testing.T) {
+	address := common.HexToAddress("0x1318ca3CD01CFa04C7D129Aa5f9cF7145736C0be")
+	scAddress := ethCrypto.CreateAddress(address, 1)
+	fmt.Printf("Meta ID Address: %x \n", scAddress)
+}
---
+++ metaservice/sc/identitymanager/wrapper.go
@@ -0,0 +1,111 @@
+package identitymanager
+
+import (
+	"fmt"
+	"math/big"
+	"sync"
+
+	"bitbucket.org/coinplugin/proxy/log"
+
+	"bitbucket.org/coinplugin/proxy/crypto"
+	"bitbucket.org/coinplugin/proxy/metaservice/sc/registry"
+	"bitbucket.org/coinplugin/proxy/rpc"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	//	"github.com/ethereum/go-ethereum/crypto"
+)
+
+var (
+	once   sync.Once
+	zero   = big.NewInt(0)
+	glimit = uint64(4000000)
+)
+
+var instance *Identitymanager
+
+func getService() (*Identitymanager, error) {
+	once.Do(func() {
+		_rpc := rpc.GetInstance()
+		client := _rpc.GetEthClient()
+		//var err error
+		imAddress, err := registry.GetIMContractAddress()
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		instance, err = NewIdentitymanager(*imAddress, client)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+	})
+
+	if instance == nil {
+		err := fmt.Errorf("Cannot make service for IdentityManager")
+		return nil, err
+	}
+	return instance, nil
+}
+
+//CallCreateMetaID createMetaID function call
+func CallCreateMetaID(mgtAddress common.Address) (*types.Transaction, error) {
+	var trx *types.Transaction
+	var err error
+
+	service, err := getService()
+	//session, err := getSession()
+
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	tx := func(nonce uint64) error {
+		_rpc := rpc.GetInstance()
+		auth := crypto.GetTransactionOpts()
+		auth.Nonce = big.NewInt(int64(nonce))
+		auth.GasPrice = big.NewInt(int64(_rpc.GetGasPrice()))
+
+		trx, err = service.CreateMetaId(auth, mgtAddress)
+		if err != nil {
+			log.Error(err)
+			return err
+		}
+		return nil
+	}
+	c := crypto.GetInstance()
+	res := c.ApplyNonce(tx)
+
+	if !res {
+		if err == nil {
+			err = fmt.Errorf("call function Error - CreateMetaID")
+		}
+		return nil, err
+	}
+
+	return trx, nil
+}
+
+func CallGetDeployedMetaIds() ([]common.Address, error) {
+	service, err := getService()
+
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	addrs, err := service.GetDeployedMetaIds(&bind.CallOpts{})
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	log.Debug("Address List: ", addrs)
+	return addrs, nil
+}
+
+/*
+func CreateAddress(b common.Address, nonce uint64) common.Address {
+    data, _ := rlp.EncodeToBytes([]interface{}{b, nonce})
+    return common.BytesToAddress(Keccak256(data)[12:])
+}
+*/
