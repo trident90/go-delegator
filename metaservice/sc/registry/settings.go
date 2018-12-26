05711aec8fbebe8ab5fea65d729ec1962473f338
신용욱 <lineart@gmail.com>
2018-12-26 14:43:05 +0900

    Update name service address

--- metaservice/sc/registry/settings.go
+++ metaservice/sc/registry/settings.go
@@ -2,4 +2,4 @@ package registry
 
 import "github.com/ethereum/go-ethereum/common"
 
-var nsAddress = common.HexToAddress("0xf6fb5669b9eff32b5ad80bb27e61a73f578f2710")
+var nsAddress = common.HexToAddress("0x29712f5fe784356f75955a884229080690887f11")
